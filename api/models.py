afda9776173d9dc2fa52227d09dd75fd58da0af5
zhiiw <1302593554@qq.com>
2021-05-31 03:15:53 +0800

    build the model

---
+++ api/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class House(models.Model):
+    priceUnit=models.IntegerField()
+    priceSum=models.IntegerField()
+
+
+class agent(models.Model):
+
+
+class position(models.Model):
+
+
+class seller(models.Model):
+
+
+class case(models.Model):
+
+class User(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    username = models.CharField(db_column='USERNAME', unique=True, max_length=20)  # Field name made lowercase.
+    password = models.CharField(db_column='PASSWORD', max_length=30)  # Field name made lowercase.
+    register_time = models.DateField(db_column='REGISTER_TIME')  # Field name made lowercase.
+    role = models.IntegerField(db_column='ROLE')  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'user'
+
+
+# Create your models here.
---
+++ api/views.py
@@ -0,0 +1,70 @@
+from django.db.models import QuerySet
+from django.shortcuts import render
+
+# Create your views here.
+import json
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import datetime
+
+from .models import User
+# Create your views here.
+
+def login(request):
+    dic = {}
+    if request.method == 'GET':
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Method"
+        return HttpResponse(json.dumps(dic))
+
+    try:
+        post_content = json.loads(request.body)
+        username = post_content['username']
+        password = post_content['password']
+        user = User.objects.get(username=username)
+    except (KeyError, json.decoder.JSONDecodeError):
+        dic['status'] = "Failed"
+        dic['message'] = "No Input"
+        return HttpResponse(json.dumps(dic))
+    except User.DoesNotExist:
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Username"
+        return HttpResponse(json.dumps(dic))
+    if user.password != password:
+        dic['message'] = "Wrong Password"
+        dic['status'] = "Failed"
+        return HttpResponse(json.dumps(dic))
+    else:
+        dic['status'] = "Success"
+        dic['user_id'] = user.uid
+        dic['user_role'] = user.role
+        return HttpResponse(json.dumps(dic))
+
+
+@csrf_exempt
+def register(request):
+    dic = {}
+    if request.method == 'GET':
+        dic['status'] = "Failed"
+        dic['message'] = "Wrong Method"
+        return HttpResponse(json.dumps(dic))
+    try:
+        post_content = json.loads(request.body)
+        username = post_content['username']
+        password = post_content['password']
+        role = post_content['role']
+        user = User.objects.get(username=username)
+    except (KeyError, json.decoder.JSONDecodeError):
+        dic['status'] = "Failed"
+        dic['message'] = "No Input"
+        return HttpResponse(json.dumps(dic))
+    except User.DoesNotExist:
+        dic['status'] = "Success"
+        now = datetime.datetime.now()
+        newUser = User(username=username, password=password, role=role, register_time=now)
+        newUser.save()
+        return HttpResponse(json.dumps(dic))
+    if user is not None:
+        dic['status'] = "Failed"
+        dic['message'] = "User exist"
+        return HttpResponse(json.dumps(dic))
